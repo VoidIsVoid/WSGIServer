27fca1ecdac623a3b7a274b11ef5e4e20ce7259e
Void <343750470@qq.com>
2016-09-26 23:29:48 +0800

    v1.1

--- 14301094/WSGIServer.py
+++ 14301094/WSGIServer.py
@@ -165,3 +165,4 @@ if __name__ == '__main__':
         httpd = make_server(SERVER_ADDRESS, application)
     print('WSGIServer: Serving HTTP on port {port} ...\n'.format(port=PORT))
     httpd.serve_forever()
+    
